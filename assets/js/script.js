25d108653574c407eeacd01887b9b8a896dbcdf7
Yann Treuiller-Schlachter <yann.treuiller.schlachter@gmail.com>
2025-02-28 16:55:04 +0100

    First

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // annuler le comportement par défaut du formulaire
+  document.querySelector("form")?.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const firstName = document.getElementById("prenom").value;
+    const lastName = document.getElementById("nom").value;
+    const email = document.getElementById("mail").value;
+    const subject = document.getElementById("sujet").value;
+    const message = document.getElementById("message").value;
+
+    const response = await axios.post(
+      "https://site--mailersend--mz8pkhlfl2x7.code.run/form",
+      {
+        firstName,
+        lastName,
+        email,
+        subject,
+        message,
+      }
+    );
+
+    console.log(response.data);
+  });
+
+  // Click sur envoyer le formulaire
+  const formButton = document.getElementById("form-button");
+
+  formButton.addEventListener("click", () => {
+    const titleToModify = document.getElementById("form-title");
+    titleToModify.textContent = "Formulaire envoyé !";
+    titleToModify.classList.add("send");
+    console.log("Formulaire envoyé");
+  });
+
+  // ouvrir le formulaire
+  const open = document.getElementById("btn-show-form");
+
+  open.addEventListener("click", () => {
+    const modal = document.querySelector(".modal");
+    modal.classList.remove("hide");
+  });
+
+  // fermer le formulaire
+  const cross = document.getElementById("close");
+
+  cross.addEventListener("click", () => {
+    const modal = document.querySelector(".modal");
+    modal.classList.add("hide");
+  });
+});
