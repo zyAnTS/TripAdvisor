c10b919db4c0b2294c7072afe0aedc810b96c9b1
Yann Treuiller-Schlachter <yann.treuiller.schlachter@gmail.com>
2025-03-02 17:58:28 +0100

    Formulaire & carousel update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const formButton = document.getElementById("form-button");
+  const formulaire = document.querySelector(".formulaire");
+  const open = document.getElementById("btn-show-form");
+  const confirmation = document.querySelector(".confirmation-await");
+  const crossFormulaire = document.getElementById("close-formulaire");
+  const crossConfirmation = document.getElementById("close-confirmation");
+
   // annuler le comportement par défaut du formulaire
-  document.querySelector("form")?.addEventListener("submit", async (event) => {
+  document.querySelector("form").addEventListener("submit", async (event) => {
     event.preventDefault();
+
+    const confirmation = document.querySelector(".confirmation-await");
+    const formulaire = document.querySelector(".formulaire");
+
+    // receuillir les infos du formulaire
 
     const firstName = document.getElementById("prenom").value;
     const lastName = document.getElementById("nom").value;
+
     // const email = document.getElementById("mail").value;
     const email = "zyan01@hotmail.com";
     const subject = document.getElementById("sujet").value;
@@ -25,27 +38,32 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Click sur envoyer le formulaire
-  const formButton = document.getElementById("form-button");
 
   formButton.addEventListener("click", () => {
-    const titleToModify = document.getElementById("form-title");
-    titleToModify.textContent = "Formulaire envoyé !";
-    titleToModify.classList.add("send");
+    formulaire.classList.add("hide");
+    confirmation.classList.add("confirmation");
     console.log("Formulaire envoyé");
   });
 
   // ouvrir le formulaire
-  const open = document.getElementById("btn-show-form");
 
-  open.addEventListener("click", (event) => {
+  open.addEventListener("click", () => {
     const modal = document.querySelector(".modal");
     modal.classList.remove("hide");
+    formulaire.classList.remove("hide");
+    confirmation.classList.remove("confirmation");
   });
 
   // fermer le formulaire
-  const cross = document.getElementById("close");
 
-  cross.addEventListener("click", () => {
+  crossFormulaire.addEventListener("click", () => {
+    const modal = document.querySelector(".modal");
+    modal.classList.add("hide");
+  });
+
+  // fermer la confirmation
+
+  crossConfirmation.addEventListener("click", () => {
     const modal = document.querySelector(".modal");
     modal.classList.add("hide");
   });
